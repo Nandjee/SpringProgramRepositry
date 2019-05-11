bea4648b2a555c67d0340e4e2454b3239a08ed8b
nandjee <tech.rahulanand1421@gmail.com>
2019-05-11 15:39:14 +0530

    added project

---
+++ OnlinShopingFrontInt/src/main/java/com/oln/shoping/OnlinShopingFrontIntApplication.java
@@ -0,0 +1,13 @@
+package com.oln.shoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinShopingFrontIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinShopingFrontIntApplication.class, args);
+	}
+
+}
